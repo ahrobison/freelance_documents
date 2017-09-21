e0dacc205243440f4cfe60ccea610c042e3d472a
ahrobison <ahrob27@gmail.com>
2017-09-21 13:05:12 -0600

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+#  create_table "freelance_documents", force: :cascade do |t|
+#     t.string "title"
+#     t.string "description"
+#     t.text "file_url"
+#     t.text "image_url"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
+# end
+
+10.times do |d|
+	FreelanceDocument.create!(title: "Document #{d}", 
+													description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+													file_url: "https://docs.google.com/document/d/1alfJzKXdfhLvzbPFp58tzBW70b0TIh5v5so38zu910Y/edit?usp=sharing",
+													image_url: "https://i.pinimg.com/736x/26/31/df/2631df7e202870fe6523ba85b2dcafcf--tropical-forest-tree-tree.jpg",
+													)
+end
